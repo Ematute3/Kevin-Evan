ec659fef387d446c4276e57bc3a4593b03601af2
Ematute3 <141678140+Ematute3@users.noreply.github.com>
2025-09-02 11:20:49 -0700

    kevin test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KevinTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "TankDrive_Iterative")
+public class KevinTest extends OpMode {
+
+    DcMotor leftMotor, rightMotor;
+
+    @Override
+    public void init() {
+        // map motors
+        leftMotor  = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+        // reverse one side so forward stick makes robot go forward
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addLine("Tank Drive Initialized");
+    }
+
+    @Override
+    public void loop() {
+        // left stick controls left motor, right stick controls right motor
+        double leftPower  = -gamepad1.left_stick_y;   // invert so up = forward
+        double rightPower = -gamepad1.right_stick_y;
+
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+
+        telemetry.addData("Left Power", leftPower);
+        telemetry.addData("Right Power", rightPower);
+    }
+}
